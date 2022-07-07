b072b77ed2da83def91a55c2bc5623f0abfa1f23
Edmar Reis <91325197+EdmarReis@users.noreply.github.com>
2022-07-07 11:14:26 -0300

    Atualizacao

--- app/src/main/java/me/dio/simulator/domain/Match.kt
+++ app/src/main/java/me/dio/simulator/domain/Match.kt
@@ -1,10 +1,16 @@
 package me.dio.simulator.domain
 
+import com.google.gson.annotations.SerializedName
+
 data class Match (
 
+    @SerializedName("Descricao")
     val description: String,
+    @SerializedName("local")
     val place: Place,
+    @SerializedName("mandante")
     val homeTeam: Team,
+    @SerializedName("visitante")
     val awayTeam: Team
 
 )
