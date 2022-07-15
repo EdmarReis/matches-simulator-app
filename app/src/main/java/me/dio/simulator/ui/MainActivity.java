05e729cbb78492601ad3a94bef85caf1254a91fa
Edmar Reis <91325197+EdmarReis@users.noreply.github.com>
2022-07-15 11:18:01 -0300

    incluindo nomes e bandeiras

--- app/src/main/java/me/dio/simulator/ui/MainActivity.java
+++ app/src/main/java/me/dio/simulator/ui/MainActivity.java
@@ -2,18 +2,17 @@ package me.dio.simulator.ui;
 
 import android.os.Bundle;
 import android.util.Log;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.snackbar.Snackbar;
-
 import java.util.List;
-
 import me.dio.simulator.R;
 import me.dio.simulator.data.MatchesAPI;
 import me.dio.simulator.databinding.ActivityMainBinding;
 import me.dio.simulator.domain.Match;
+import me.dio.simulator.ui.adapter.MatchesAdapter;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -24,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private MatchesAPI matchesApi;
+    private RecyclerView.Adapter matchesAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -76,6 +76,26 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupMatchesList() {
         //TODO listar as partidas, consumindo nossa API
+        binding.rvMatches.setHasFixedSize(true);
+        binding.rvMatches.setLayoutManager(new LinearLayoutManager(this));
+        matchesApi.getMatches().enqueue(new Callback<List<Match>>() {
+            @Override
+            public void onResponse(Call<List<Match>> call, Response<List<Match>> response) {
+                if(response.isSuccessful()){
+                    List<Match> matches = response.body();
+                    matchesAdapter = new MatchesAdapter(matches);
+                    binding.rvMatches.setAdapter(matchesAdapter);
+                }else{
+                    showErrorMessage();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Match>> call, Throwable t) {
+
+            }
+        });
+
     }
 
     private void showErrorMessage() {
